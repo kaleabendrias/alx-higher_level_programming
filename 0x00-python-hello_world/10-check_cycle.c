2b32bcb28a574e9b4bd4cd96e52b866a86ef6199
kaleabendrias <kaleabendrias1212@gmail.com>
2023-06-05 07:11:18 -0700

    cycle check

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if the list has cycle or not
+ * @list: list
+ * Return: Always
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *one = list;
+	listint_t *two = list;
+
+	while (two != NULL && two->next != NULL)
+	{
+		one = one->next;
+		two = two->next;
+		if (one == two)
+			return (1);
+	}
+	return (0);
+}
