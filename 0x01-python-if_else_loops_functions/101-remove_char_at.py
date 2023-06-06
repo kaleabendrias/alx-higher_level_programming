45b1a0172c4b0f5059308f627db92553bdf62836
kaleabendrias <kaleabendrias1212@gmail.com>
2023-06-06 08:16:03 -0700

    prints the ASCII alphabet, in reverse order, alternating lowercase and uppercase

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    new_str = str[:n] + str[n + 1:]
+    if (n >= 0):
+        new_str = str[:n] + str[n + 1:]
+    else:
+        return (str)
     return (new_str)
