ba7b5b8aa5df116d50c3df5d6784d36a7d7b162c
kaleabendrias <kaleabendrias1212@gmail.com>
2023-06-08 02:11:30 -0700

    printsing arguments passed script"

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    length = len(sys.argv) - 1
+    if length == 0 or length > 1:
+        print("{} arguments.".format(length))
+    else:
+        print("{} argument.".format(length))
+    if length >= 1:
+        length = 0
+        for arg in sys.argv:
+            if length != 0:
+                print("{}: {}".format(length, arg))
+            length += 1
