1194077efc3c51bf155c22a3f76048b70b5043b5
kaleabendrias <kaleabendrias1212@gmail.com>
2023-06-08 04:22:42 -0700

    repaired codes based on pycodestyle

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,15 +2,11 @@
 if __name__ == "__main__":
     import sys
     length = len(sys.argv) - 1
-    if length == 0 :
+    if length == 0:
         print("{} arguments.".format(length))
-    elif length ==1:
+    elif length == 1:
         print("{} argument:".format(length))
     else:
         print("{} arguments:".format(length))
-    if length >= 1:
-        length = 0
-        for arg in sys.argv:
-            if length != 0:
-                print("{}: {}".format(length, arg))
-            length += 1
+    for i in range(length):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
