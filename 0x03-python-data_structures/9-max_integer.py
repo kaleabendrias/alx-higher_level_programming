82e82fafa1fcff17d9080a7abf0a6fb59aed7d6a
kaleabendrias <kaleabendrias1212@gmail.com>
2023-06-09 02:07:54 -0700

    fixed pycode style

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,8 +4,7 @@ def max_integer(my_list=[]):
     if (len(my_list) == 0):
         return (None)
     for i in range(length - 1):
-        for j in range(0, length - i -1):
+        for j in range(0, length - i - 1):
             if (my_list[j] > my_list[j + 1]):
                 my_list[j], my_list[j + 1] = my_list[j + 1], my_list[j]
     return (my_list[-1])
-
