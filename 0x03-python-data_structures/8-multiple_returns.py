dcfa7eef8d1d78f441dc5374a8b99d92c4f47351
kaleabendrias <kaleabendrias1212@gmail.com>
2023-06-12 00:20:35 -0700

    fix

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,8 +2,9 @@
 def multiple_returns(sentence):
     length = len(sentence)
     first = sentence[0]
+    new_t = ()
     if (length == 0):
-        new_t = (0, "None")
+        new_t = 0, "None"
     else:
         new_t = (length, first)
     return (new_t)
