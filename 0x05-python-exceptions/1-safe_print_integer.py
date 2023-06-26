e245d53ab74894ce4bbb9183cc76f6dc4826fecb
kaleabendrias <kaleabendrias1212@gmail.com>
2023-06-26 02:23:41 -0700

    fix

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if (type(value) == int):
-            print("{:d}".format(value))
-            return (True)
-        else:
-            return (False)
+        print("{:d}".format(value))
+        return (True)
     except:
-        pass
+        return (False)
