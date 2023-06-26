d7b70eafbe34066bbb2bd6c4f6e06a62b8a25268
kaleabendrias <kaleabendrias1212@gmail.com>
2023-06-26 08:51:35 -0700

    py byte code

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-result = 0
-for i in range(1, 4):
-    try:
-        if (i > a):
-            raise Exception("too far")
-        result += (a / b) ** i
-    except Exception:
-        result = b + a
-        break
-return (result)
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 4):
+        try:
+            if (i > a):
+                raise Exception("too far")
+            result += (a / b) ** i
+        except Exception:
+            result = b + a
+            break
+    return (result)
