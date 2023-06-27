8d32d14daf0d32d6df97d835073b0ac368e9b686
kaleabendrias <kaleabendrias1212@gmail.com>
2023-06-27 02:26:02 -0700

    repaired disc

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 """defines a sqaure class"""
 class Square:
-    """Empty class that is class square"""
-    def __init__(self):
-        """initialize a new instance"""
-         pass
+    """inititlaxie pass"""
+    pass
