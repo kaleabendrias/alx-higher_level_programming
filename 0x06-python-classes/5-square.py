e20899e54623b8d288a63734e4a875f3f4cd5951
kaleabendrias <kaleabendrias1212@gmail.com>
2023-06-27 03:31:22 -0700

    added docume

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
+"""sqaure class that has getter,setter and prints the sqaure with '#'"""
+
+
 class Square:
+    """define square"""
     def __init__(self, size=0):
         self.__size = size
 
     @property
     def size(self):
+        """getter for size"""
         return (self.__size)
 
     @size.setter
     def size(self, value):
+        """setter for varibale size"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -16,9 +22,11 @@ class Square:
         self.__size = value
 
     def area(self):
+        """returns area of square:"""
         return (self.__size ** 2)
 
     def my_print(self):
+        """prints square with '#'"""
         if self.__size == 0:
             print()
         else:
