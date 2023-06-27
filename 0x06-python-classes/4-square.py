95bd989f7b9ec0c5919e8562ad824e983889d41a
kaleabendrias <kaleabendrias1212@gmail.com>
2023-06-27 03:28:21 -0700

    repair docum

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
+"""square class that has getter and setter for its size and returns area"""
+
+
 class Square:
+    """define square"""
     def __init__(self, size=0):
+        """intialize """
         self.__size = size
 
     @property
     def size(self):
+        """getter for size"""
         return (self.__size)
 
     @size.setter
     def size(self, value):
+        """setter for size"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -16,4 +23,5 @@ class Square:
         self.__size = value
 
     def area(self):
+        """return area"""
         return (self.__size ** 2)
