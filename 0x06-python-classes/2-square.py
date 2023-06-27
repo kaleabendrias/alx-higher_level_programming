f867ae511630c03ae79745e1ba9e36b8510f5a46
kaleabendrias <kaleabendrias1212@gmail.com>
2023-06-27 02:32:46 -0700

    repair docum

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""define a class Square"""
+
+
 class Square:
+    """initialize it"""
     def __init__(self, size=0):
         if type(size) != int:
             raise TypeError("size must be an integer")
