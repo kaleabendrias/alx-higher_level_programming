681ae8c637ca3f421bf8e8a1e478070d4b9b3370
kaleabendrias <kaleabendrias1212@gmail.com>
2023-06-30 00:23:03 -0700

    finished the function

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,1 +1,28 @@
-x
+#!/usr/bin/python3
+"""module for dividing matrix"""
+
+
+
+def matrix_divided(matrix, div):
+    """divide all elemetns of a mtrix by div"""
+    if not isinstance(matrix, list):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if any(not isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    row_sizes = set(len(row) for row in matrix)
+    if len(row_sizes) > 1:
+        raise TypeError("Each row of the matrix must have the same size")
+
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    new_matrix = []
+    for row in matrix:
+        new_row = [round(element / div, 2) for element in row]
+        new_matrix.append(new_row)
+
+    return (new_matrix)
