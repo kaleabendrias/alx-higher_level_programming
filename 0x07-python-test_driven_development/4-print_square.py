f1ecb1220cf1b72a4a141b1d3e4606586b91a546
kaleabendrias <kaleabendrias1212@gmail.com>
2023-07-02 09:54:25 -0700

    finishde the function and test

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,1 +1,13 @@
-x
+#!/usr/bin/python3
+""" prints # only accepts integer"""
+
+
+
+def print_square(size):
+    """prints square"""
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for _ in range(size):
+        print("#" * size)
