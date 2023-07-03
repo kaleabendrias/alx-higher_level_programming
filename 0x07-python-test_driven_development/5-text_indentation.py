3bba7fbe6979721e755b2bc96b242b03c1cba533
kaleabendrias <kaleabendrias1212@gmail.com>
2023-07-03 09:12:58 -0700

    started the test

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,18 @@
-x
+#!/usr/bin/python3
+""" prints new line if it incounters ., ?, :."""
+
+
+def text_indentation(text):
+    """ prints newline after getting some chars"""
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    lines = text.splitlines()
+    result = ""
+    characters = [".", "?", ":"]
+    for line in lines:
+        stripped = line.rstrip()
+        for char in stripped:
+            result += char.strip()
+            if char in characters:
+                result += "\n\n"
+    print(result.rstrip())
