ac051d2f7cf6eb962c4d61cd9cccc4db9e2a8b1e
kaleabendrias <kaleabendrias1212@gmail.com>
2023-07-03 19:52:04 +0300

    started the func

---
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -0,0 +1,32 @@
+def matrix_mul(m_a, m_b):
+    if not isinstance(m_a, list):
+        raise TypeError("m_a must be a list")
+    
+    if not isinstance(m_b, list):
+        raise TypeError("m_b must be a list")
+
+    if not all(isinstance(row, list) for row in m_a):
+        raise TypeError("m_a must be a list of lists")
+    if not all(isinstance(row, list) for row in m_b):
+        raise TypeError("m_b must be a list of lists")
+
+    if len(m_a) == 0:
+        raise ValueError("m_a can't be empty")
+    if len(m_b) == 0:
+        raise ValueError("m_b can't be empty")
+
+    if not all(isinstance(num, (int, float)) for row in m_a for num in row):
+        raise TypeError("m_a should contain only integers or floats")
+    if not all(isinstance(num, (int, float)) for row in m_b for num in row):
+        raise TypeError("m_b should contain only integers or floats")
+
+    if len(set(len(row) for row in m_a)) > 1:
+        raise TypeError("each row of m_a must be of the same size")
+    if len(set(len(row) for row in m_b)) > 1:
+        raise TypeError("each row of m_b must be of the same size")
+    if len(m_a[0]) != len(m_b):
+        raise ValueError("m_a and m_b can't be multiplied")
+
+    result = [[sum(a * b for a, b in zip(row_a, col_b)) for col_b in zip(*m_b)] for row_a in m_a]
+    return result
+
