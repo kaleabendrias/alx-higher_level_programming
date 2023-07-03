1c75b2d54568b381d019e2c0240606e0a85f9484
kaleabendrias <kaleabendrias1212@gmail.com>
2023-07-03 20:22:53 +0300

    added the test ffile

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,25 +1,20 @@
 def matrix_mul(m_a, m_b):
     if not isinstance(m_a, list):
         raise TypeError("m_a must be a list")
-    
     if not isinstance(m_b, list):
         raise TypeError("m_b must be a list")
-
     if not all(isinstance(row, list) for row in m_a):
         raise TypeError("m_a must be a list of lists")
     if not all(isinstance(row, list) for row in m_b):
         raise TypeError("m_b must be a list of lists")
-
     if len(m_a) == 0:
         raise ValueError("m_a can't be empty")
     if len(m_b) == 0:
         raise ValueError("m_b can't be empty")
-
     if not all(isinstance(num, (int, float)) for row in m_a for num in row):
         raise TypeError("m_a should contain only integers or floats")
     if not all(isinstance(num, (int, float)) for row in m_b for num in row):
         raise TypeError("m_b should contain only integers or floats")
-
     if len(set(len(row) for row in m_a)) > 1:
         raise TypeError("each row of m_a must be of the same size")
     if len(set(len(row) for row in m_b)) > 1:
