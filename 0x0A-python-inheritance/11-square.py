cd4848dd248a58aa90779020671acf0efc8551e2
kaleabendrias <kaleabendrias1212@gmail.com>
2023-07-10 09:11:30 -0700

    finished func

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Square modue"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """inherted from rectangle"""
+
+    def __init__(self, size):
+        """init method"""
+        self.integer_validator('size', size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def __str__(self):
+        """PRINT ETHOD"""
+        return "[{}] {}/{}".format(__class__.__name__, self.__size, self.__size)
