d035783ea8effda2fa6a7d542ead5449d1f97abf
kaleabendrias <kaleabendrias1212@gmail.com>
2023-07-10 02:21:40 -0700

    finished the func

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""class module"""
+
+
+class BaseGeometry:
+    """ class Geometry"""
+
+    def area(self):
+        """raise exp area isnot implemented"""
+        raise Exception("area() is not implemented")
