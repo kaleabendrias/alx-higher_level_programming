74397bb4d65fb3b0195ef1f1eed6afefad02075a
kaleabendrias <kaleabendrias1212@gmail.com>
2023-07-10 09:33:02 -0700

    fixed

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -13,5 +13,5 @@ class BaseGeometry:
         """chrck if value is type int"""
         if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
+        elif value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
