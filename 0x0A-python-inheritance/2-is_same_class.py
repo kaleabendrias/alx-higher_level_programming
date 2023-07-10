e3903f7fd20180551ecbf05033be69ddb59d05db
kaleabendrias <kaleabendrias1212@gmail.com>
2023-07-10 01:13:13 -0700

    fix

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/in/python3
+""" checks class"""
+
+
+def is_same_class(obj, a_class):
+    """checks if same tyoe of obj"""
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
