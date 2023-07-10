7174f73758a347b480ae56786852ece461c6a72c
kaleabendrias <kaleabendrias1212@gmail.com>
2023-07-10 09:27:32 -0700

    finshed func

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -4,6 +4,6 @@
 
 def add_attribute(obj, objname, value):
     """adds atrr to obj else raises ex"""
-    if hasattr(obj, "__dict__") == False:
+    if hasattr(obj, "__dict__") is False:
         raise TypeError("can't add new attribute")
     setattr(obj, objname, value)
