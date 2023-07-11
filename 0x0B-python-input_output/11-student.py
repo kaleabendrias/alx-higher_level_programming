b10aa2ea04315aba7af15f68162c1f8f74a5a032
kaleabendrias <kaleabendrias1212@gmail.com>
2023-07-11 13:19:12 -0700

    fix

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""class Student"""
+
+
+class Student:
+    """class student"""
+
+    def __init__(self, first_name, last_name, age):
+        """init method"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """returns disc discription"""
+        if isinstance(attrs, list) and all(isinstance(attr, str)
+                for attr in attrs):
+            res = {}
+            for i in attrs:
+                if i in self.__dict__:
+                    res[i] = self.__dict__[i]
+            return res
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """replace atrr"""
+        for k, v in json.items():
+            setattr(self, k, v)
