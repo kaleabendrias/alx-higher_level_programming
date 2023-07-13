f2770623ba76e77566a94797cabb3cc914dd6b0d
kaleabendrias <kaleabendrias1212@gmail.com>
2023-07-13 00:56:08 -0700

    finished function deisplay with out handling x and y

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -77,3 +77,11 @@ class Rectangle(Base):
     def area(self):
         """calculates area"""
         return self.__width * self.__height
+
+    def display(self):
+        """display rectangle using #"""
+        for i in range(0, self.__height):
+            for j in range(0, self.__width):
+                print("#", end="")
+            print()
+
