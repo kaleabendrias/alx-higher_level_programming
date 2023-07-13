17fe8cce9a1fe9940668bf5c57f2f26b83e0a1d2
kaleabendrias <kaleabendrias1212@gmail.com>
2023-07-13 00:03:47 -0700

    fixed x and added control

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -32,24 +32,44 @@ class Rectangle(Base):
     @property
     def y(self):
         """getter fr y"""
-        return self.__x
+        return self.__y
 
     @width.setter
     def width(self, value):
         """width setter"""
 
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @height.setter
     def height(self, value):
         """height setter"""
+
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @x.setter
     def x(self, value):
         """setter for x"""
+
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value <= 0:
+            raise ValueError("x must be > 0")
         self.__x = value
 
     @y.setter
     def y(self, value):
+        """setter for y"""
+
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value <= 0:
+            raise ValueError("y must be > 0")
         self.__y = value
