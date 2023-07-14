41c20998aa831d606022cd39c6233b47b0b46164
kaleabendrias <kaleabendrias1212@gmail.com>
2023-07-14 00:35:56 -0700

    fix

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -94,3 +94,16 @@ class Rectangle(Base):
         n = f"[{type(self).__name__}] ({self.id}) {self.__x}/{self.__y}\
                 - {self.__width}/{self.__height}"
         return n
+
+    def update(self, *args):
+        """gives varibles value from args"""
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) > 1:
+            self.width = args[1]
+        if len(args) > 2:
+            self.height = args[2]
+        if len(args) > 3:
+            self.x = args[3]
+        if len(args) > 4:
+            self.y = args[4]
