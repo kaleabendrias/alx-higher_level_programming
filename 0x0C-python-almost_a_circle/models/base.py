fa722f6f0ebc7e8940c831446e5669aee17eccc7
kaleabendrias <kaleabendrias1212@gmail.com>
2023-07-15 21:36:58 -0700

    fix

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,8 @@
 """class base"""
 
 import json
+import os
+import csv
 
 
 class Base:
@@ -48,4 +50,68 @@ class Base:
                 raise TypeError("json_string must be a string")
             li = json.loads(json_string)
         return li
-        
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with allattr set"""
+        if cls.__name__ == 'Rectangle':
+            n = cls(1, 1)
+        elif cls.__name__ == 'Square':
+            n = cls(1)
+        n.update(**dictionary)
+        return n
+
+    @classmethod
+    def load_from_file(cls):
+        """returns li of insta"""
+        fi = cls.__name__ + ".json"
+        li = []
+        lis_dic = []
+        if os.path.exists(fi):
+            with open(fi, 'r') as f:
+                s = f.read()
+                lis_dic = cls.from_json_string(s)
+                for d in lis_dic:
+                    li.append(cls.create(**d))
+        return li
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """serilize in csv"""
+        if (type(list_objs) != list and list_objs is not None or
+                not all(isinstance(x, cls) for x in list_objs)):
+            raise TypeError("list_objs must be a list of instances")
+
+        fi = cls.__name__ + ".csv"
+        with open(fi, 'w') as f:
+            if list_objs is not None:
+                list_objs = [x.to_dictionary() for x in list_objs]
+                if cls.__name__ == 'Rectangle':
+                    fields = ['id', 'width', 'height', 'x', 'y']
+                elif cls.__name__ == 'Square':
+                    fields = ['id', 'size', 'x', 'y']
+                writer = csv.DictWriter(f, fieldnames=fields)
+                writer.writeheader()
+                writer.writerows(list_objs)
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """deserialize csv"""
+
+        fi = cls.__name__ + ".csv"
+        li = []
+        if os.path.exists(fi):
+            with open(fi, 'r') as f:
+                reader = csv.reader(f, delimiter=',')
+                if cls.__name__ == 'Rectangle':
+                    fields = ['id', 'width', 'height', 'x', 'y']
+                elif cls.__name__ == 'Square':
+                    fields = ['id', 'size', 'x', 'y']
+                for x, row in enumerate(reader):
+                    if x > 0:
+                        i = cls(1, 1)
+                        for j, e in enumerate(row):
+                            if e:
+                                setattr(i, fields[j], int(e))
+                        li.append(i)
+        return li
