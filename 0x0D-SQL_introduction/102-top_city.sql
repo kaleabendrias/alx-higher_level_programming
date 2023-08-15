eee55eeacc831b19db17c28ed4c0803836c5a29b
kaleabendrias <kaleabendrias1212@gmail.com>
2023-08-15 09:39:57 -0700

    finished

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,7 @@
+-- script that displays the average temperature (Fahrenheit) by city 
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+WHERE month = 7 or month = 8
+GROUP BY city
+ORDER BY avg_temp DESC
+LIMIT 3;
