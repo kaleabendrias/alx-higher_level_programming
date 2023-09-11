585118df037d1e152cc4dce2a217cc397a3d808e
kaleabendrias <kaleabendrias1212@gmail.com>
2023-09-11 12:43:20 +0300

    finished

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 // script that prints x times “C is fun”
 
-const arg = process.argv[2];
+const arg = process.argv;
 
-const x = parseInt(arg, 10);
+const x = parseInt(arg[2], 10);
 
-if (!isNaN(x) && x > 0) {
-  for (let i = 0; i < x; i++) {
+if (!isNaN(x)) {
+  for (let i = 0; i < args[2]; i++) {
     console.log('C is fun');
   }
 } else {
