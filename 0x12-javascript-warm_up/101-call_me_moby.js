8b13966d267db3c0c06d7a6834ae1f2aafd450d9
kaleabendrias <kaleabendrias1212@gmail.com>
2023-09-11 14:40:47 +0300

    finished

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 // func that executes x times a func.
 
-function callMeMoby(x, y){
-    for (let i = 0; i < x; i++) {
-        y();
-    }
+function callMeMoby (x, y) {
+  for (let i = 0; i < x; i++) {
+    y();
+  }
 }
 
 module.exports = {
-    callMeMoby: callMeMoby
+  callMeMoby
 };
