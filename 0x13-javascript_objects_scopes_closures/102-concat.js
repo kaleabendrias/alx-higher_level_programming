081032fc8af43f5d228637095b20126392645f78
kaleabendrias <kaleabendrias1212@gmail.com>
2023-09-12 11:04:32 +0300

    finished

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const sourceFile1 = fs.readFileSync(process.argv[2], 'utf8');
+const sourceFile2 = fs.readFileSync(process.argv[3], 'utf8');
+fs.writeFileSync(process.argv[4], sourceFile1 + sourceFile2);
