1ef5dc931ce0cdf5c598023a614929dcc4346782
kaleabendrias <kaleabendrias1212@gmail.com>
2023-09-12 10:18:51 +0300

    finished

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -4,5 +4,5 @@ const data = require('./100-data.js');
 
 const originalList = data.list;
 
-console.log(originalList.list);
-console.log(originalList.list.map((x, idx) => x * idx));
+console.log(originalList);
+console.log(originalList.map((x, idx) => x * idx));
