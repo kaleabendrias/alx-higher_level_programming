1d178f52da9b53630a7dbf4c102e28d0acdb3067
kaleabendrias <kaleabendrias1212@gmail.com>
2023-09-12 09:08:08 +0300

    finished

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -3,6 +3,6 @@
 
 module.exports = class square extends require('./4-rectangle') {
   constructor (size) {
-    super(size, size); // call the super class's constructor with the arguments passed in.
+    super(size, size);
   }
 };
