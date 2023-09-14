fc9287926673a3134ed032ba72d189d629e29978
kaleabendrias <kaleabendrias1212@gmail.com>
2023-09-14 19:20:19 +0300

    finished the script

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""script that changes the name of a State"""
+
+import sys
+from model_city import Base, City
+from model_state import State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
+    usern, passwd, db = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(usern, passwd, db), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Retrieve the State object with id 2 and update its name
+    citites = session.query(City).order_by(City.id).all()
+    for city in citites:
+        state = session.query(State).filter(State.id == city.state_id).first()
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    session.close()
+    
