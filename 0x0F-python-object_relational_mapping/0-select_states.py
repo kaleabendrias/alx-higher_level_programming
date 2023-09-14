b1f3e9a2a19bb655a0a5cdf8e9309c5838e04447
kaleabendrias <kaleabendrias1212@gmail.com>
2023-09-14 10:33:48 +0300

    finished the script

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+'''script that lists all states from the database hbtn_0e_0_usa'''
+import MySQLdb
+import sys
+
+
+def main():
+    # makes conn and executes query
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+        
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    
+    # Connect to the MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        user=username,
+        passwd=password,
+        db=database,
+        port=3306
+    )
+
+    # Create a cursor object
+    cursor = db.cursor()
+    
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+    
+    cursor.close()
+    db.close()
+    
+if __name__ == "__main__":
+    from sys import argv
+    main()
