b0aec8782d38eb5492cafb8279d2c2c1f530d0be
kaleabendrias <kaleabendrias1212@gmail.com>
2023-09-29 19:20:57 +0300

    finished

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,7 @@ import urllib.request
 url = "https://alx-intranet.hbtn.io/status"
 with urllib.request.urlopen(url) as res:
     html = res.read()
-    print("Body response:$")
-    print(f'\t - type: {type(res)}')
-    print(f'\t - content: {html}')
-    print(f"\t - utf8 content:", html.decode("utf-8"))
+    print("Body response:")
+    print(f'\t- type: {type(res)}')
+    print(f'\t- content: {html}')
+    print(f"\t- utf8 content: ", html.decode("utf-8"))
---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+#  a Python script that takes your GitHub credentials 
+import requests
+import sys
+from requests.auth import HTTPBasicAuth
+
+url = "https://api.github.com/user"
+user_name = sys.argv[1]
+passwd = sys.argv[2]
+credentials = (user_name, passwd)
+res = requests.get(url, auth=credentials)
+print(res)
