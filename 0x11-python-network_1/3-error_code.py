3fd32df9c4425cd01c0ece6e403c2d92be419b73
kaleabendrias <kaleabendrias1212@gmail.com>
2023-09-29 16:52:02 +0300

    finished

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,9 +4,9 @@ import urllib.request
 import sys
 
 url = sys.argv[1]
-with urllib.request.urlopen(url) as res:
-    body = res.read()
-    if body:
+try:
+    with urllib.request.urlopen(url) as res:
+        body = res.read()
         print(body.decode('utf-8'))
-    else:
-        print(f"Error code: {res.status}")
+except urllib.error.HTTPError as e:
+    print(f"Error code: {e.code}")
