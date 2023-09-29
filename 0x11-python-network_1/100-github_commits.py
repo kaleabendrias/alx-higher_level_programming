9f442208e3f6327627346cff6a276861fec29b32
kaleabendrias <kaleabendrias1212@gmail.com>
2023-09-29 20:07:01 +0300

    finished

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# Python script that takes 2 arguments
+import requests
+import sys
+
+repo = sys.argv[1]
+owner = sys.argv[2]
+url = f"https://api.github.com/repos/{owner}/{repo}/commits"
+
+res = requests.get(url).json()
+for i in range(10):
+    print(f"{res[i]['sha']}: {res[i]['commit']['author']['name']}")
