126ca8a1091f665adb4e11617f5c7dd1ead3c0aa
kaleabendrias <kaleabendrias1212@gmail.com>
2023-09-29 17:48:28 +0300

    finished

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# a Python script that takes in a URL and an email address
+import requests
+import sys
+
+url = sys.argv[1]
+email = sys.argv[2]
+res = requests.get(url, data=email)
+print(res)
