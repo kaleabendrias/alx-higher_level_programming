38c1b844b20c28f535fbd6895fca99a6f755e909
kaleabendrias <kaleabendrias1212@gmail.com>
2023-09-29 16:44:05 +0300

    finished

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# a Python script that takes in a URL, sends a request to the URL
+import urllib.request
+import sys
+
+url = sys.argv[1]
+with urllib.request.urlopen(url) as res:
+    body = res.body
+    print(body)
