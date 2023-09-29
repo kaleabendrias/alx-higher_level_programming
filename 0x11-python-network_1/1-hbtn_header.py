58f925b6c444f6d7fdf5150933faa0a0b5064cab
kaleabendrias <kaleabendrias1212@gmail.com>
2023-09-29 15:53:35 +0300

    finished

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+#  Python script that takes in a URL, sends a request to the URL
+import urllib.request
+import sys
+
+url = sys.argv[1]
+with urllib.request.urlopen(url) as res:
+    body = res.headers
+    print(body['X-Request-Id'])
