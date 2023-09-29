6f0b56f10833c07991298c4f4f75dafddebf439b
kaleabendrias <kaleabendrias1212@gmail.com>
2023-09-29 15:32:06 +0300

    finished

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# Write a Python script that fetches https://alx-intranet.hbtn.io/status
+import urllib.request
+
+url = "https://alx-intranet.hbtn.io/status"
+with urllib.request.urlopen(url) as res:
+    html = res.read()
+    print("Body response:$")
+    print(f'\t - type: {type(res)}')
+    print(f'\t - content: {html}')
+    print(f"\t - utf8 content:", html.decode("utf-8"))
