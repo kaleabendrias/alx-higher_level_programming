2683a78a324359a08d961a97766aaf50c0889083
kaleabendrias <kaleabendrias1212@gmail.com>
2023-09-30 12:32:42 +0300

    finished

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,6 +5,6 @@ import requests
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
     res = requests.get(url)
-    print("Body response:$")
+    print("Body response:")
     print(f"\t- type: {type(res.text)}")
     print(f"\t- content: {res.content.decode('utf-8')}")
