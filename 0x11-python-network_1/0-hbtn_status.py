069b918e813f4ad4fcade0f8f293d7ed01561294
kaleabendrias <kaleabendrias1212@gmail.com>
2023-09-30 11:39:07 +0300

    finished

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,11 +2,11 @@
 # Write a Python script that fetches https://alx-intranet.hbtn.io/status
 import urllib.request
 
-if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as res:
-        html = res.read()
-        print("Body response:")
-        print(f'\t- type: {type(res)}')
-        print(f'\t- content: {html}')
-        print(f"\t- utf8 content: ", html.decode("utf-8"))
+
+url = "https://alx-intranet.hbtn.io/status"
+with urllib.request.urlopen(url) as res:
+    html = res.read()
+    print("Body response:")
+    print(f'\t- type: {type(res)}')
+    print(f'\t- content: {html}')
+    print(f"\t- utf8 content: ", html.decode("utf-8"))
