9727e44e2768e1d894488e5f4a6c8549f8882ac0
kaleabendrias <kaleabendrias1212@gmail.com>
2023-09-30 11:54:36 +0300

    finished

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,4 +9,4 @@ with urllib.request.urlopen(url) as res:
     print("Body response:")
     print(f'\t- type: {type(html)}')
     print(f'\t- content: {html}')
-    print(f"\t- utf8 content: ", html.decode("utf-8"))
+    print(f"\t- utf8 content:", html.decode("utf-8"))
