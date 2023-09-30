a107520e0943ce553906e01958eb8cf3dfd21695
kaleabendrias <kaleabendrias1212@gmail.com>
2023-09-30 13:49:42 +0300

    finished

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,10 +5,10 @@ import sys
 
 if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
-    if len(sys.argv) == 2:
-        letter = sys.argv[1]
-    else:
+    if len(sys.argv) == 1:
         letter = ''
+    else:
+        letter = sys.argv[1]
     res = requests.post(url, data={'q': letter})
     try:
         json_res = res.json()
