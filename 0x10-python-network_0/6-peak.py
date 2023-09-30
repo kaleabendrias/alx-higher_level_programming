2dc63fa02f067f77e0cefa10baa1ae1f1ffdbc51
kaleabendrias <kaleabendrias1212@gmail.com>
2023-09-30 14:08:23 +0300

    finished

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,18 +2,14 @@
 """function that finds a peak in a list of unsorted integers"""
 
 def find_peak(list_of_integers):
+    """Find a peak in a list of unsorted integers."""
     if not list_of_integers:
         return None
 
-    left, right = 0, len(list_of_integers) - 1
+    for idx, num in enumerate(list_of_integers):
+        left = list_of_integers[idx - 1] if idx > 0 else float('-inf')
+        right = list_of_integers[idx + 1] if idx < len(list_of_integers) - 1 else float('-inf')
 
-    while left < right:
-        mid = (left + right) // 2
-
-        if list_of_integers[mid] > list_of_integers[mid + 1]:
-            right = mid
-        else:
-            left = mid + 1
-
-    return list_of_integers[left]
+        if num >= left and num >= right:
+            return num
 
