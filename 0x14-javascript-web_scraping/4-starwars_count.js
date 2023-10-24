0b801635b1ecf4d22442ad5f0a5209fcf3aabfc1
kaleabendrias <kaleabendrias1212@gmail.com>
2023-10-24 16:41:32 +0300

    finished

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,14 +1,9 @@
 #!/usr/bin/node
 const request = require('request');
+const url = process.argv[2];
 
-const apiUrl = process.argv[2];
-const characterId = '18';
-
-request.get(apiUrl, (error, response, body) => {
-  if (!error && response.statusCode === 200) {
-    const count = body.split("https://swapi-api.alx-tools.com/api/people/18").length - 1
-    console.log(count)
-  } else {
-    console.error(error);
-  }
+request(url, (err, res, body) => {
+  if (err) console.log(err);
+  const count = body.split('/people/18/').length - 1;
+  console.log(count);
 });
