755a518d3bec0df2a6bafb0282126f4d43deeafc
kaleabendrias <kaleabendrias1212@gmail.com>
2023-10-24 16:43:28 +0300

    finished

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
 const request = require('request');
-const url = process.argv[2];
 
-request(url, (err, res, body) => {
-  if (err) console.log(err);
-  const count = body.split('/people/18/').length - 1;
-  console.log(count);
+const apiUrl = process.argv[2];
+
+request.get(apiUrl, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    const count = body.split('people/18').length - 1;
+    console.log(count);
+  } else {
+    console.error(error);
+  }
 });
