dc35be64f5ddb9ae4ae0dd3a0872ef3ac0772f43
kaleabendrias <kaleabendrias1212@gmail.com>
2023-10-24 16:26:11 +0300

    finished

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -8,11 +8,14 @@ request.get(apiUrl, (error, response, body) => {
   if (!error && response.statusCode === 200) {
     const data = JSON.parse(body);
     let count = 0;
-    data.results.forEach(film => {
-      if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
+    let i = 0;
+    while (i < data.results.length) {
+      if (data.results[i].characters
+        .includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
         count++;
       }
-    });
+      i++;
+    }
     console.log(count);
   } else {
     console.error(error);
