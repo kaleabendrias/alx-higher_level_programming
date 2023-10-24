23ecf45087e391deeff7991728ca68c0401174ef
kaleabendrias <kaleabendrias1212@gmail.com>
2023-10-24 18:25:03 +0300

    finished

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -7,19 +7,19 @@ const promisifiedRequest = util.promisify(request.get);
 const url = 'https://swapi-api.hbtn.io/api/films/';
 
 (async () => {
-    try {
-        const body = (await promisifiedRequest(url + id)).body;
-        const film = JSON.parse(body);
-        for (const characterUrl of film.characters) {
-            try {
-                const characterBody = (await promisifiedRequest(characterUrl)).body;
-                const character = JSON.parse(characterBody);
-                console.log(character.name);
-            } catch (err) {
-                console.error(err);
-            }
-        }
-    } catch (err) {
+  try {
+    const body = (await promisifiedRequest(url + id)).body;
+    const film = JSON.parse(body);
+    for (const characterUrl of film.characters) {
+      try {
+        const characterBody = (await promisifiedRequest(characterUrl)).body;
+        const character = JSON.parse(characterBody);
+        console.log(character.name);
+      } catch (err) {
         console.error(err);
+      }
     }
+  } catch (err) {
+    console.error(err);
+  }
 })();
